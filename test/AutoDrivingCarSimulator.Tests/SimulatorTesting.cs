c3a7c4fdd0af565089b736e8bcf1a2a086c3d010
Dasun <dasunkasthury@gmail.com>
2025-05-31 17:23:17 +0800

    test: add possible unit tests

---
+++ test/AutoDrivingCarSimulator.Tests/SimulatorTesting.cs
@@ -0,0 +1,69 @@
+﻿using AutoFixture.Xunit2;
+
+namespace AutoDrivingCarSimulator.Tests;
+
+public class SimulatorTesting
+{
+    [Theory, InlineAutoData(5, 10)]
+    public void GivenFieldCoordinates_Validate_Coordinates(int width, int height)
+    {
+
+    }
+
+    [Theory, InlineAutoData(5, 10)]
+    public void GivenCarDetails_Validate_CarDetails(CarDto car)
+    {
+
+    }
+
+    [Theory, InlineAutoData("A")]
+    public void GivenCarName_Validate_CarName(int name)
+    {
+
+    }
+
+
+
+    [Theory, InlineAutoData("FFRRLL")]
+    public void GivenCommand_Validate_Command(string command)
+    {
+
+    }
+
+    public void GivenAddNewCar_Validate_CarList(CarDto car)
+    {
+
+    }
+
+    public void GivenCarDetails_Validate_CarDestination(CarDto car, FieldDto field)
+    {
+
+        
+    }
+
+    public void GivenCarDetails_Validate_CarCollition(IList<CarDto> carList, FieldDto field)
+    {
+
+     
+    }
+
+
+    public void GivenExit_Validate_AppStatus()
+    {
+
+      
+    }
+
+    public void GivenAddCarOption_Validate_AppNextStep()
+    {
+
+       
+    }
+
+    public void GivenCarDetails_beyondBoundary_Validate_CarDestination(CarDto car, FieldDto field)
+    {
+
+
+    }
+
+}
